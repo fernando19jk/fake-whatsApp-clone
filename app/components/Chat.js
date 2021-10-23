8d608f319d0f2b753365f433baba7fef47f2febb
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2021-10-23 19:21:36 +0200

    Remote Data funcionando

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,20 +1,43 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import {View, Text, FlatList, ActivityIndicator} from "react-native";
 import axios from 'axios';
 import { FAKE_CHATS } from '../data/data.js'
 
 export default class Chat extends Component {
-    render() {
-        return (
-            <View><Text>Chat</Text></View>
-        );
+    constructor(props) {
+        super(props)
+        this.state = {
+            chatList : [],
+            loaded: false
+        }
     }
+
+    componentDidMount() {
+        axios.get(FAKE_CHATS)
+        .then((response) => {
+         this.setState({
+             chatList: response.data,
+             loaded: true
+         })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+    render() {
+        if(this.state.loaded) {
+            return(
+                <FlatList
+                    data={this.state.chatList}
+                    renderItem={({item}) => <Text>{item.first_name} </Text>}
+                    keyExtractor={item => item.id}
+                />
+            )
+          } else {
+            return(
+                <ActivityIndicator size='large' />
+            )
+          }       
+        }
 }
 
-/*constructor(props){
-super(props)
-    this.state = {
-        chatList : [],
-        loaded: false
-    }
-}*/
