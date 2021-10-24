55254dabbea0060b62d0efc3af15d5e1e2177ed5
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2021-10-24 19:27:56 +0200

    En llamadas SALE NOMBRE

--- app/components/Call.js
+++ app/components/Call.js
@@ -1,10 +1,44 @@
+import axios from "axios";
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import {View, Text, FlatList, ActivityIndicator} from "react-native";
+import { FAKE_CALLS } from "../data/data";
 
 export default class Call extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            callList: [],
+            loaded: false
+        }
+    }
+
+    componentDidMount() {
+        axios.get(FAKE_CALLS)
+        .then((response) => {
+            this.setState({
+                callList: response.data,
+                loaded: true
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     render() {
-        return (
-            <View><Text>Call</Text></View>
-        );
+        if(this.state.loaded) {
+        return(
+            <FlatList
+              data={this.state.callList}
+              renderItem={({item}) => <Text>{item.first_name}</Text>}
+              keyExtractor={item => item.id}
+            />
+        )
+    } else {
+        return(
+            <ActivityIndicator size="large" />
+        )
     }
 }
+}
+
